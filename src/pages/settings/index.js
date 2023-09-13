12d2b3ea3c0d87a4b7de6bf312bdcf003e06f29c
shanjida Maria <shanjida@amromed.org>
2023-09-13 12:17:32 +0600

    test purpose

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -2,7 +2,7 @@ import RootLayout from "@/component/Layouts/RootLayout";
 import React from "react";
 
 const settingPage = () => {
-  return <div>still on developing ..........</div>;
+  return <div>still on developing .......... testing git hub push</div>;
 };
 
 export default settingPage;

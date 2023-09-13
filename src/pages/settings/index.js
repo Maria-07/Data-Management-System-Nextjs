11ce92730e1baa4281000a8cebae8297751e4907
shanjidaMaria <144672005+shanjidaMaria@users.noreply.github.com>
2023-09-13 12:25:28 +0600

    test no 2

--- src/pages/settings/index.js
+++ src/pages/settings/index.js
@@ -2,7 +2,11 @@ import RootLayout from "@/component/Layouts/RootLayout";
 import React from "react";
 
 const settingPage = () => {
-  return <div>still on developing .......... testing git hub push</div>;
+  return (
+    <div>
+      still on developing .......... testing git hub push again test issue
+    </div>
+  );
 };
 
 export default settingPage;
